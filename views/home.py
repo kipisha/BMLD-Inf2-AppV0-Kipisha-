3d4edd5104758a449ec036f2879c165b24f91a7f
kipisha <selvakip@students.zhaw.ch>
2026-03-04 13:05:32 +0100

    hih

--- views/home.py
+++ views/home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("Meine erste Streamlit App")
 
-# !! WICHTIG: Eure Emails müssen in der App erscheinen!!
+
 
 """
 Diese App wurde von folgenden Personen entwickelt:
@@ -12,5 +12,5 @@ Diese App wurde von folgenden Personen entwickelt:
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
-Autor: Samuel Wehrli (wehs@zhaw.ch)
+
 """
