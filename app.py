0ee2a0d8fe7da595a87b0d46a73197a9c0544bcc
kipisha <selvakip@students.zhaw.ch>
2026-03-12 19:17:33 +0100

    data manager installiert

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import streamlit as st
+from utils.data_manager import DataManager  
+from utils.login_manager import LoginManager
+
 
 
 if 'data_df' not in st.session_state:
-    st.session_state['data_df'] = pd.DataFrame()
-    
+    st.session_state['data_df'] = data_manager.load_user_data(
+        'data.csv', 
+        initial_value=pd.DataFrame(),
+        parse_dates=['timestamp']
+    )
 
 pg = st.navigation([
     st.Page("views/home.py", title="Home", icon="🏠"),
@@ -15,3 +21,9 @@ pg = st.navigation([
 
 pg.run()
 
+data_manager =DataManager(
+    fs_protocol= 'webdav'
+    fs_root_folder="BMLD_APP_DB"
+)
+login_manager = LoginManager(data_manager)
+login_manager.login_register
